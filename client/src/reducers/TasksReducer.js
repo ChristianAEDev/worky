f464fd85369c0fc3172ce35bbc12b4d0b001ef5e
Christian <ChristianAE.DEV@gmail.com>
2018-03-23 21:36:22 +0000

    Add action to add a task #15

--- client/src/reducers/TasksReducer.js
+++ client/src/reducers/TasksReducer.js
@@ -5,8 +5,6 @@ import { DELETE_TASK, GET_TASKS } from '../actions';
 export default function (state = {}, action) {
   switch (action.type) {
     case DELETE_TASK:
-      console.log(action.payload.data);
-
       return _.omit(state, action.payload.data);
     case GET_TASKS:
       return _.mapKeys(action.payload.data, 'id');
